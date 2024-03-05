c85d4a7a026fd1516c26bf03afdd5ffe104e933a
Roberto Ríos <roberto@deveahub.com>
2024-03-05 12:46:07 +0100

    feat: add typescript

--- config/database.ts
+++ config/database.ts
@@ -84,6 +84,7 @@ module.exports = ({ env }) => {
         filename: path.join(
           __dirname,
           "..",
+          "..",
           env("DATABASE_FILENAME", ".tmp/data.db")
         ),
       },
